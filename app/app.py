7dcecce9e2ea3714b9b2c9c32d3d65a71a2393fc
Tuba Balci <tubalci33@gmail.com>
2025-03-06 22:55:05 +0300

    Revert "Merge branch 'userPrefBack'"
    This reverts commit a97a28178b5019615f41eaa13b8fc22b18346421, reversing
    changes made to c267fd7d3785b19bffa5481771df13d35eca8ee8.

--- app/app.py
+++ app/app.py
@@ -3,11 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from server.routes.food import router as FoodRouter
 from server.routes.user import router as UserRouter
 from server.routes.comment import router as UserCommentsRouter
-from server.routes.auth import router as AuthRouter
-from server.routes.survey import router as SurveyRouter
-
-from server.database import database
-
+#from server.routes.translation import router as TranslationRouter
 import uvicorn
 
 app = FastAPI()
@@ -24,13 +20,11 @@ app.add_middleware(
 app.include_router(FoodRouter, prefix="/food", tags=["Food"])
 app.include_router(UserRouter, prefix="/users", tags=["User"])
 app.include_router(UserCommentsRouter, prefix="/comments", tags=["Comment"])
+#app.include_router(TranslationRouter, prefix="/translation", tags=["Translation"])
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
 
-app.state.database = database
-
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
-
--- app/config.py
+++ app/config.py
@@ -5,4 +5,3 @@ load_dotenv()  # Load environment variables from .env file
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "fuudiy")
-SECRET_KEY = os.getenv("SECRET_KEY")
